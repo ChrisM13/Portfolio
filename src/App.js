886f1ea11f3eb0a55e770f9bface8a486d083f3b
Chris Mosier <chrismosier13@gmail.com>
2017-12-07 17:31:05 -0800

    add components, set up routes, add/test background video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home/Home'
+import Resume from './components/Resume/Resume'
+import Work from './components/Work/Work'
+import About from './components/About/About'
+import Extra from './components/Extra/Extra'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <video type="video/mp4" autoPlay loop id="video-background" muted>
+          <source src="assets/video/portfolioVid.mp4" />
+        </video>
+        <Router>
+          <div className="container">
+            <NavBar />
+              <Switch>
+                <Route path='/resume' component={Resume}/>
+                <Route path='/work' component={Work}/>
+                <Route exact path='/' component={Home}/>
+                <Route path='/about' component={About}/>
+                <Route path='/extra' component={Extra}/>
+              </Switch>
+            </div>
+        </Router>
       </div>
     );
   }
