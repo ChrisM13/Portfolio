4c667569c6e49614596d50ac3c5b96c8f46610d2
Chris Mosier <chrismosier13@gmail.com>
2017-12-20 19:00:15 -0800

    ready to rollback to previous commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ import Home from './components/Home/Home'
 import Resume from './components/Resume/Resume'
 import Work from './components/Work/Work'
 import About from './components/About/About'
-import Extra from './components/Extra/Extra'
 import database from './config/database'
+import Typed from 'typed.js'
+import $ from 'jquery'
 
 class App extends Component {
   constructor() {
@@ -20,7 +21,35 @@ class App extends Component {
       projects: database
     }
   }
-
+  componentDidMount() {
+    $(function(){
+      $("#typedTop").typed({
+        strings: ["Hello!"],
+        typeSpeed: 50,
+        showCursor: false
+      });
+    });
+    $(function(){
+      $("#typedBottom").typed({
+        strings: ["I'm a full-stack ^500 web developer."],
+        typeSpeed: 100,
+        startDelay: 1500,
+        showCursor: false
+      });
+    });
+    
+    // $("#typedTop").typed({
+    //   strings: ["Hello!"],
+    //   typeSpeed: 50,
+    //   showCursor: false
+    // })
+    
+    // $("#typedBottom").typed({
+    //   strings: ["I'm a full-stack ^500 web developer."],
+    //   typeSpeed: 100,
+    //   startDelay: 1500,
+    //   showCursor: false
+  }
   render() {
     return (
       <div className="App">
@@ -36,7 +65,6 @@ class App extends Component {
                   }/>
                 <Route exact path='/' component={Home}/>
                 <Route path='/about' component={About}/>
-                <Route path='/extra' component={Extra}/>
               </Switch>
             </div>
         </Router>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Typed from 'typed.js';
+import './Home.css'
+
+
+
 
 const Home = () => {
     return (
         <div>
+            <div class="typed_wrap">
+                <h1><span id="typedTop"></span></h1>
+                <h1><span id="typedBottom"></span></h1>
+            </div>
             <video type="video/mp4" autoPlay loop id="video-background" muted>
                 <source src="assets/video/portfolioVid.mp4" />
             </video>
