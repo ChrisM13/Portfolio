5563ef27adbfd16c6cf5b10c8ecf0a3688133eb9
Chris Mosier <chrismosier13@gmail.com>
2017-12-21 10:45:17 -0800

    ready to redeploy to gh-pages

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,13 +3,21 @@ import {Link} from 'react-router-dom';
 import {Navbar} from 'react-materialize'
 import './NavBar.css';
 
+function onClick(event) {
+  event.preventDefault();
+  return (
+    <Link to="/"></Link>
+  )
+}
+
 const NavBar = (props) => {
   return (
-    <Navbar className="black navbar" brand='Chris Mosier' left fixed>
+    <Navbar className="black navbar" brand="" left fixed onClick={onClick}>    
       <ul>
-        <li><Link to="/about" className='NavBar-link'>About</Link></li>
-        <li><Link to="/resume" className='NavBar-link'>Resume</Link></li>
+        <li><Link to="/" className='NavBar-link Chris'>Chris Mosier</Link></li>
         <li><Link to="/work" className='NavBar-link'>Work</Link></li>
+        <li><Link to="/resume" className='NavBar-link'>Resume</Link></li>
+        <li><Link to="/about" className='NavBar-link'>About</Link></li>
       </ul>
     </Navbar>
   );
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Resume.css'
+import {Button} from 'react-materialize'
 
 const Resume = () => {
     return (
         <div className="resume">
-            Resume
+            <Button className="black"><a href="../../public/assets/pdf/Resume2018.pdf" download>Download Resume</a></Button>
         </div>
     )
 }
